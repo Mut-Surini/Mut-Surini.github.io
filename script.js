cbada363c598d44f7c183fee8982662ebe8d38da
Mutsurini <fitrahqareem99@gmail.com>
2022-06-13 23:07:33 +0700

    new file:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+const nameProduct = [{
+	'Name' : 'Uranus',
+	'Id' : 0,
+	'Price' : 7000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Sport'
+},{
+	'Name' : 'Sultan',
+	'Id' : 1,
+	'Price' : 20000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Sport'
+},{
+	'Name' : 'BF-400',
+	'Id' : 2,
+	'Price' : 3000,
+	'Categories' : 'MotorCycle',
+	'Sub-cat' : 'Sport'
+},{
+	'Name' : 'FreeWay',
+	'Id' : 3,
+	'Price' : 2000,
+	'Categories' : 'MotorCycle',
+	'Sub-cat' : 'Iconic'
+},{
+	'Name' : 'Boxville',
+	'Id' : 4,
+	'Price' : 2000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Commerce'
+},{
+	'Name' : 'Wayfarer',
+	'Id' : 5,
+	'Price' : 1500,
+	'Categories' : 'MotorCycle',
+	'Sub-cat' : 'Reguler'
+},{
+	'Name' : 'Elegy',
+	'Id' : 6,
+	'Price' : 9000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Sport-Car'
+},{
+	'Name' : 'Taxi',
+	'Id' : 7,
+	'Price' : 2000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Commerce'
+}]
