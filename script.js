5112b9b8854d1049796595cf23097c9856ff0f1a
Mutsurini <fitrahqareem99@gmail.com>
2022-06-15 21:48:50 +0700

    Decide to SansFransisco Showroom and design html with bootstrap5

--- script.js
+++ script.js
@@ -1,49 +1,65 @@
 const nameProduct = [{
-	'Name' : 'Uranus',
+	'Name' : 'Bullet',
 	'Id' : 0,
+	'Price' : 80000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Sport'
+	'Desk' : 'The Bulllet is a modern car that has a flexible design and its color combination makes it unique as a cool racing car',
+	'StockSelled' : 30
+},{
+	'Name' : 'Faggio',
+	'Id' : 1,
+	'Price' : 4000,
+	'Categories' : 'MotorCycle',
+	'Sub-cat' : 'Antique',
+	'Desk' : 'Faggio is an antique motorcycle that has been around for a long time, with its simple design making it attractive to antique vehicle owners',
+	'StockSelled' : 10
+},{
+	'Name' : 'Huntley',
+	'Id' : 2,
+	'Price' : 2500,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Reguler',
+	'Desk' : 'The Huntley is a family car that is comfortable to use and with the ability to withstand a wide variety of climates',
+	'StockSelled' : 18
+},{
+	'Name' : 'Infernus',
+	'Id' : 3,
+	'Price' : 150000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Sport',
+	'Desk' : 'The Infernus is a premium class car that is suitable to be used as a famous car as well as a sports car, with a fairly modern design that makes this car go fast.',
+	'StockSelled' : 12
+},{
+	'Name' : 'NRG-500',
+	'Id' : 4,
+	'Price' : 90000,
+	'Categories' : 'MotorCycle',
+	'Sub-cat' : 'Sport',
+	'Desk' : 'The NRG-500 is a sports motorbike that can go very fast, has a high-class design and components that make this motorbike a motorbike that is in demand by wealthy collectors.',
+	'StockSelled' : 21
+},{
+	'Name' : 'RoadTrain',
+	'Id' : 5,
+	'Price' : 3000,
+	'Categories' : 'Car',
+	'Sub-cat' : 'Commercial',
+	'Desk' : 'Roadtrain is a commercial type car that works for trucker workers who can carry various types of cargo for distribution',
+	'StockSelled' : 8
+},{
+	'Name' : 'Slamvan',
+	'Id' : 6,
 	'Price' : 7000,
 	'Categories' : 'Car',
-	'Sub-cat' : 'Sport'
+	'Sub-cat' : 'Antique',
+	'Desk' : 'The slamvan is a type of antique lowrider car that is difficult to drive because of its slippery tire components but has a design that is suitable for collection.',
+	'StockSelled' : 6
 },{
-	'Name' : 'Sultan',
-	'Id' : 1,
-	'Price' : 20000,
-	'Categories' : 'Car',
-	'Sub-cat' : 'Sport'
-},{
-	'Name' : 'BF-400',
-	'Id' : 2,
-	'Price' : 3000,
-	'Categories' : 'MotorCycle',
-	'Sub-cat' : 'Sport'
-},{
-	'Name' : 'FreeWay',
-	'Id' : 3,
-	'Price' : 2000,
-	'Categories' : 'MotorCycle',
-	'Sub-cat' : 'Iconic'
-},{
-	'Name' : 'Boxville',
-	'Id' : 4,
-	'Price' : 2000,
-	'Categories' : 'Car',
-	'Sub-cat' : 'Commerce'
-},{
-	'Name' : 'Wayfarer',
-	'Id' : 5,
-	'Price' : 1500,
-	'Categories' : 'MotorCycle',
-	'Sub-cat' : 'Reguler'
-},{
-	'Name' : 'Elegy',
-	'Id' : 6,
-	'Price' : 9000,
-	'Categories' : 'Car',
-	'Sub-cat' : 'Sport-Car'
-},{
-	'Name' : 'Taxi',
+	'Name' : 'Yosemite',
 	'Id' : 7,
-	'Price' : 2000,
+	'Price' : 3000,
 	'Categories' : 'Car',
-	'Sub-cat' : 'Commerce'
+	'Sub-cat' : 'Reguler',
+	'Desk' : 'Yosemite is a vehicle that is suitable for use in various situations ranging from carrying various kinds of goods to having a car body that is quite tough and strong.',
+	'StockSelled' : 32
 }]
